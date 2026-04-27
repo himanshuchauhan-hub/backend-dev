dfdf2985da9633373687ef9bf6faed7578ab808f
Himanshu Chauhan <himanshuchauhan.hc16@gmail.com>
2026-04-27 17:20:12 +0530

    utils files

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+class ApiResponse {
+  constructer(statusCode, data, message = "success"){
+    this.statusCode = statusCode
+    this.data = data
+    this.message = message
+    this.success = statusCode < 400
+  }
+}
+
+
+export { ApiResponse }
