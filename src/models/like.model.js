a5b2616a0f1b8adb74f278d0a7af054a3ec7d920
Himanshu Chauhan <himanshuchauhan.hc16@gmail.com>
2026-05-01 22:22:22 +0530

    feat: add models for Comment, Like, Playlist, and Tweet with necessary schemas

---
+++ src/models/like.model.js
@@ -0,0 +1,24 @@
+import mongoose, {Schema} from "mongoose";
+
+
+const likeSchema = new Schema({
+  video: {
+    type: Schema.Types.ObjectId,
+    ref: "Video"
+  },
+  likedBy: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  tweet: {
+    type: Schema.Types.ObjectId,
+    ref: "Tweet"
+  },
+  comment: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }
+})
+
+
+export const Like = mongoose.model("Like", likeSchema)
